5ed6a1de52c2012061e8f05acb54c43ffafb0843
Safeer Ahmed <pakistanireacts9@gmail.com>
2024-11-19 22:08:44 +0500

    Updated RandomNumber.ts
    Added Code for Guess Random Number

--- RandomNumber.ts
+++ RandomNumber.ts
@@ -1,3 +1,32 @@
-console.log("Hello World");
+//Importing Module from the inquirer
+import inquirer from "inquirer";   
+
+// Taking input from the user
+// 1.) Computer will Generate a random number
+// 2.) Take number from the user to guess the number
+// 3.) User number and Computer number comparision
+
+// Random Number Generation
+const randNumber = Math.floor(Math.random() * 10 + 1);
+console.log(randNumber);
+
+// Taking Input from the user
+const answers = await inquirer.prompt([
+    {
+        name: "userNumber", 
+        type: "number", 
+        message:"I have a number Let me know if you know it : "
+    }
+ ]);
+
+// // Comparing Condition for the Computer number vs User input
+if(answers.userNumber == randNumber)
+    {
+        console.log(`Congrats! you have guessed right. Number which is ${answers.userNumber}`);
+    }
+     else
+    {
+         console.log(`Sorry! your guess is wrong. Number is ${answers.userNumber}`);
+    }
 
 
